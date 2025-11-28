ee0e965f4a6708d772a5decec4f2648a24e7481d
HongDat <33374303+HongTanDat1810@users.noreply.github.com>
2025-11-28 14:36:42 +0700

    Create LanguageContext.tsx

---
+++ contexts/LanguageContext.tsx
@@ -0,0 +1,41 @@
+
+import React, { createContext, useState, useContext, useEffect, ReactNode } from 'react';
+import { translations, Language } from '../utils/translations';
+
+interface LanguageContextType {
+  language: Language;
+  setLanguage: (lang: Language) => void;
+  t: typeof translations['en'];
+}
+
+const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
+
+export const LanguageProvider = ({ children }: { children: ReactNode }) => {
+  const [language, setLanguage] = useState<Language>('vn'); // Default to Vietnamese
+
+  useEffect(() => {
+    const savedLang = localStorage.getItem('hrm_language') as Language;
+    if (savedLang && (savedLang === 'en' || savedLang === 'vn')) {
+      setLanguage(savedLang);
+    }
+  }, []);
+
+  const handleSetLanguage = (lang: Language) => {
+    setLanguage(lang);
+    localStorage.setItem('hrm_language', lang);
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage: handleSetLanguage, t: translations[language] }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext);
+  if (context === undefined) {
+    throw new Error('useLanguage must be used within a LanguageProvider');
+  }
+  return context;
+};
